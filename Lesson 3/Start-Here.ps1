e4f3a7adcaa06130fb285341533c6088b9c2e638
garywige <91767400+garywige@users.noreply.github.com>
2022-01-21 15:06:59 -0800

    created start-here script

---
+++ Lesson 3/Start-Here.ps1
@@ -0,0 +1,13 @@
+# script arguments
+# functions etc
+
+<#
+    ENTRY POINT
+#>
+
+# print title
+# prompt for source (if not specified by argument)
+# prompt for destination (if not specified by argument)
+# prompt for copy mode (if not specified by argument)
+# business logic
+# final output
