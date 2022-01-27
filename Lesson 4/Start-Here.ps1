729bea3d0ef5bea3e994bc27ea3f87eca4cbebff
garywige <91767400+garywige@users.noreply.github.com>
2022-01-27 12:18:04 -0800

    start-here.ps1 created script

---
+++ Lesson 4/Start-Here.ps1
@@ -0,0 +1,15 @@
+# script parameters
+# variables
+# functions
+# classes
+
+<#
+    ENTRY POINT
+#>
+# print title
+# prompt user for start IP address
+# prompt for end IP address
+# validate that end IP address comes after start IP address
+# generate tests
+# start the tests
+# output the result
